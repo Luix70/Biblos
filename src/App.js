9497ba5ee4c1e79a0345f87c439c8c9c2c45564d
Luix70 <lortuno1@gmail.com>
2020-06-30 12:57:33 +0200

    refactorizado con clases

--- src/App.js
+++ src/App.js
@@ -1,49 +1,7 @@
 import React from "react";
-import { getData } from "./components/datosWeb.js";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Image from "react-bootstrap/Image";
-
-async function handleInputChange(event) {
-  const val = event.target.value;
-  const rval = val.replace(/-/g, "");
-  if (rval.length === 10 || rval.length === 13) {
-    const result = await getData(rval);
-    console.log(rval);
-    console.log(result);
-    const items = result.totalItems;
-    const titulo =
-      items > 0 ? result.items[0].volumeInfo.title : "No Encontrado";
-
-    const cover =
-      items > 0 ? result.items[0].volumeInfo.imageLinks.thumbnail : "";
-
-    document.getElementById("OutputISBN").value = titulo;
-    document.getElementById("cover").src = cover;
-  }
-}
+import Buscador from "./components/buscador.jsx";
 function App() {
-  return (
-    <Container className="p-3">
-      <Form>
-        <div class="form-group">
-          <label for="exampleInputISBN">ISBN</label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputISBN"
-            aria-describedby="ISBNHelp"
-            onChange={handleInputChange}
-          />
-          <small id="ISBNHelp" class="form-text text-muted">
-            Introduce o escanea el ISBN
-          </small>
-          <input type="text" class="form-control mt-4 vh-50" id="OutputISBN" />
-          <Image id="cover" src="" fluid />
-        </div>
-      </Form>
-    </Container>
-  );
+  return <Buscador></Buscador>;
 }
 
 export default App;
---
+++ src/components/buscador.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { getData } from "./datosWeb.js";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+
+class Buscador extends Component {
+  handleInputChange = async (event) => {
+    const val = event.target.value;
+    const rval = val.replace(/-/g, "");
+    if (rval.length === 10 || rval.length === 13) {
+      const result = await getData(rval);
+
+      this.setState({ result });
+
+      const items = result.totalItems;
+      const titulo =
+        items > 0 ? result.items[0].volumeInfo.title : "No Encontrado";
+
+      const cover =
+        items > 0 ? result.items[0].volumeInfo.imageLinks.thumbnail : "";
+
+      document.getElementById("OutputISBN").value = titulo;
+      document.getElementById("cover").src = cover;
+    }
+  };
+
+  state = { result: {} };
+
+  render() {
+    //console.log(this.state.result);
+
+    const { totalItems, items } = this.state.result;
+
+    var libro = totalItems ? items[0] : { title: "", details: "" };
+
+    return (
+      <Container className="p-3">
+        <Form>
+          <div className="form-group">
+            <label htmlFor="exampleInputISBN">ISBN</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputISBN"
+              aria-describedby="ISBNHelp"
+              onChange={this.handleInputChange}
+            />
+            <small id="ISBNHelp" className="form-text text-muted">
+              Introduce o escanea el ISBN
+            </small>
+
+            <input
+              type="text"
+              className="form-control mt-4 vh-50"
+              id="OutputISBN"
+              value={libro.title}
+            />
+            <Image id="cover" src="" fluid />
+          </div>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default Buscador;
